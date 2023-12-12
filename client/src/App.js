afa0510a1193e0cbe706271475148eff8cf068fa
qnguyen95 <quangtri.nguyen@dxc.com>
2023-12-12 12:07:23 +0700

    #Setup Front End

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import {Routes, Route} from 'react-router-dom'
+import { Authentication } from './Components/Auth/Authentication';
+import { HomePage } from './Components/HomePage/HomePage';
+
+function App() {
+  return (
+    <div className="">
+        <Routes>
+            <Route path='/' element={true?<HomePage/>:<Authentication/>}></Route>
+        </Routes>
+    </div>
+  );
+}
+
+export default App;
